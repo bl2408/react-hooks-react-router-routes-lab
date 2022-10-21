0a10c844179ae7763d3d3022b8baa1149ed96027
Brian Lambert <brianlambert2408@gmail.com>
2022-10-21 17:56:03 +1100

    Completed tasks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const NavLinkStyle ={
+  backgroundColor: "transparent",
+  padding: "10px",
+  margin: "5px"
+};
+
+const ActiveNavLinkStyle ={
+  backgroundColor: "red"
+};
+
+
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+
+  const links = ["Movies", "Directors", "Actors"];
+
+  return <div className="navbar">
+      <NavLink activeStyle={ActiveNavLinkStyle} style={NavLinkStyle} exact to="/">Home</NavLink>
+      {links.map(link=><NavLink key={link} activeStyle={ActiveNavLinkStyle} style={NavLinkStyle} to={`/${link.toLowerCase()}`}>{link}</NavLink>)}
+    </div>;
 }
 
 export default NavBar;
